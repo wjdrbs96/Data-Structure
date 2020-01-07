5df9b9fea54925d42fd8633146a8cb81b9436207
최정균 <wjdrbs966@naver.com>
2020-01-07 16:45:59 +0900

    병합 정렬 자바로 구현하기

---
+++ 정렬/MergeSort.java
@@ -0,0 +1,52 @@
+package Algorithm;
+
+public class MergeSort {
+    static int[] sorted = new int[8];
+
+    public static void merge(int list[], int left, int mid, int right){
+        int i = left, j = mid+1, k = left, l;  //static 메소드의 변수가 좀 헷갈리네
+
+        while(i<=mid && j<=right) {
+            if (list[i] <= list[j]) {
+                sorted[k++] = list[i++];
+            } else {
+                sorted[k++] = list[j++];
+            }
+        }
+
+        if(i > mid){
+            for(l=j; l<=right; l++){
+                sorted[k++] = list[l];
+            }
+        }
+        else{
+            for(l=i; l<=mid; l++){
+                sorted[k++] = list[i];
+            }
+        }
+
+        for(l=left; l<=right; l++){
+            list[l] = sorted[l];
+        }
+    }
+
+    public static void merge_sort(int list[], int left, int right){
+        int mid;
+        if(left < right){
+            mid = (left + right)/2;
+            merge_sort(list, left, mid);
+            merge_sort(list,mid+1, right);
+            merge(list,left,mid,right);
+        }
+    }
+
+    public static void main(String[] args){
+        int[] list = {27,10,12,20,25,13,15,22};
+        merge_sort(list,0,list.length-1);
+
+        for(int q=0; q<list.length; q++){
+            System.out.print(list[q] + " ");
+        }
+
+    }
+}
