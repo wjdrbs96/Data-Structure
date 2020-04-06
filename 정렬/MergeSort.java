62a9af9a65ed0ab2e55180e3e24a61fcc3912e68
최정균 <wjdrbs966@naver.com>
2020-04-06 14:20:03 +0900

    merge Sort Update

--- 정렬/MergeSort.java
+++ 정렬/MergeSort.java
@@ -4,7 +4,7 @@ public class MergeSort {
     static int[] sorted = new int[8];
 
     public static void merge(int list[], int left, int mid, int right){
-        int i = left, j = mid+1, k = left, l;
+        int i = left, j = mid + 1, k = left, l;
 
         while(i <= mid && j <= right) {
             if (list[i] <= list[j]) {
@@ -14,37 +14,37 @@ public class MergeSort {
             }
         }
 
-        if(i > mid){
-            for(l = j; l <= right; l++){
+        if (i > mid){
+            for (l = j; l <= right; l++){
                 sorted[k++] = list[l];
             }
         }
-        else{
-            for(l = i; l <= mid; l++){
+        else {
+            for (l = i; l <= mid; l++){
                 sorted[k++] = list[l];
             }
         }
 
-        for(l = left; l <= right; l++){
+        for (l = left; l <= right; l++){
             list[l] = sorted[l];
         }
     }
 
     public static void merge_sort(int list[], int left, int right){
         int mid;
-        if(left < right){
+        if (left < right){
             mid = (left + right) / 2;
             merge_sort(list, left, mid);
-            merge_sort(list,mid+1, right);
+            merge_sort(list,mid + 1, right);
             merge(list, left, mid, right);
         }
     }
 
     public static void main(String[] args){
         int[] list = {27, 10, 12, 20, 25, 13, 15, 22};
-        merge_sort(list,0,list.length-1);
+        merge_sort(list,0,list.length - 1);
 
-        for(int q = 0; q < list.length; q++){
+        for (int q = 0; q < list.length; q++){
             System.out.print(list[q] + " ");
         }
 
