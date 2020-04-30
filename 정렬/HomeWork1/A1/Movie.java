b7d88a38fba0360a01d006da20265aac94e04bb3
최정균 <wjdrbs966@naver.com>
2020-04-30 14:42:01 +0900

    코드수정

--- 정렬/HomeWork1/A1/Movie.java
+++ 정렬/HomeWork1/A1/Movie.java
@@ -10,35 +10,31 @@ public class Movie implements Comparable {
     }
 
 
-    @Override                                // 정렬 기준 1 (제목순으로 정렬)
+    /*@Override                                // 정렬 기준 1 (제목순으로 정렬)
     public int compareTo(Object o) {
         Movie m1 = (Movie)o;
 
         return this.title.compareTo(m1.title);
-    }
+    }*/
 
     /*@Override                               // 정렬 기준 2 (연도순으로 정렬)
     public int compareTo(Object o) {
         Movie m1 = (Movie)o;
-        Integer a = this.year;
-        Integer b = m1.year;
 
-        return a.compareTo(b);
+        return this.year - m1.year;
     }*/
 
-    /*@Override                             // 정렬 기준 3 (1. 연도순으로 정렬 -> 2. 연도가 같으면 제목 순)
+    @Override                             // 정렬 기준 3 (1. 연도순으로 정렬 -> 2. 연도가 같으면 제목 순)
     public int compareTo(Object o) {
         Movie m1 = (Movie)o;
-        Integer a = this.year;
-        Integer b = m1.year;
 
-        if (a.compareTo(b) == 0) {
+        if (this.year == m1.year) {
             return this.title.compareTo(m1.title);
         }
 
-        return a.compareTo(b);
+        return this.year - m1.year;
 
-    }*/
+    }
 
     @Override
     public String toString() {
